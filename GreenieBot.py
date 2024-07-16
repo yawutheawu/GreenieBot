cf3db5e50008773bb17b9f77cc5413952632a238
yawutheawu <mayklprime@gmail.com>
2024-07-16 10:35:39 +0200

    Sheet Getter Function + Some addition global variables

--- GreenieBot.py
+++ GreenieBot.py
@@ -2,8 +2,20 @@ import discord
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import json
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import SheetGetter
+
+SheetsAPIFile = Path(r"Sensitive/Hidden/SheetsAPI.json")
 environmentFile = Path(r"Sensitive/Hidden/Variables.env")
+TokenPath = Path(r"Sensitive/Hidden/token.json")
 load_dotenv(environmentFile)
 BotAPI = os.getenv("DiscordAPIKey")
-SheetsAPI = os.getenv("SheetsAPIKey")
 SheetLink = os.getenv("SheetLink")
+SheetID = SheetLink.split("/")[7]
+TrapSheetRange = "'Trap Sheet'!A:I"
